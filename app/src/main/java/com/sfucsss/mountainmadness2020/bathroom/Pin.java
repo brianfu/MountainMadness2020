2877dc757f6f684e2e0bc0556b2df3d014b8e316
Natchan03 <natchan_ca@hotmail.com>
2020-02-22 16:41:28 -0800

    Hardcode coordinates

---
+++ app/src/main/java/com/sfucsss/mountainmadness2020/bathroom/Pin.java
@@ -0,0 +1,13 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+public class Pin {
+    double longitude;
+    double latitude;
+    char letter;
+
+    Pin(double longitude, double latitude, char letter) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.letter = letter;
+    }
+}
