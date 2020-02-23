0b63c3e1ec985d6cd4b66ef8a76e644b9e627850
abehnia <ardavan.behnia@gmail.com>
2020-02-22 22:35:05 -0800

    Test for Nate

---
+++ app/src/test/java/com/sfucsss/mountainmadness2020/bathroom/PinTest.java
@@ -0,0 +1,12 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PinTest {
+    @Test
+    public void check() {
+        assertEquals(1, 1);
+    }
+}
