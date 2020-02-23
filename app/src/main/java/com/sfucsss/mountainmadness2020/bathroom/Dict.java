ca859fd466f711e395b5b5b0f4ff90edae0c9b67
abehnia <ardavan.behnia@gmail.com>
2020-02-22 19:56:19 -0800

    dictionary added

---
+++ app/src/main/java/com/sfucsss/mountainmadness2020/bathroom/Dict.java
@@ -0,0 +1,24 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Dict {
+    private HashSet<String> dictionary;
+
+    public Dict() {
+        dictionary = new HashSet<String>();
+        File file = new File("dictionary.txt");
+        Scanner input = new Scanner("dictionary.txt");
+        while (input.hasNextLine()) {
+            dictionary.add(input.nextLine());
+        }
+    }
+    public Dict(HashSet<String> dictionary) {
+        this.dictionary = dictionary;
+    }
+    public boolean isValid(String word) {
+        return dictionary.contains(word);
+    }
+}
