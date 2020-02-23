36c0e2937c2612e255ddd226486a1569bbeb845f
abehnia <ardavan.behnia@gmail.com>
2020-02-22 20:53:29 -0800

    Adding interfaces

---
+++ app/src/main/java/com/sfucsss/mountainmadness2020/bathroom/Dict_I.java
@@ -0,0 +1,7 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+import java.util.HashSet;
+
+public interface Dict_I {
+    public boolean isValid(String word);
+}
---
+++ app/src/main/java/com/sfucsss/mountainmadness2020/bathroom/Stats_I.java
@@ -0,0 +1,12 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+import java.util.ArrayList;
+
+public interface Stats_I {
+    public double currentScore();
+    public double totalTime();
+    public double totalDistance();
+    public ArrayList<String> words();
+    public boolean contains(Checkpoint checkpoint);
+    public void update(Checkpoint checkpoint);
+}
