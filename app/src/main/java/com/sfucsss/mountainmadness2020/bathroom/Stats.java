ce529c110faabb34172bfbc6fc57f9f64b7cd32f
abehnia <ardavan.behnia@gmail.com>
2020-02-22 18:18:08 -0800

    Stats added

---
+++ app/src/main/java/com/sfucsss/mountainmadness2020/bathroom/Stats.java
@@ -0,0 +1,45 @@
+package com.sfucsss.mountainmadness2020.bathroom;
+
+import com.sfucsss.mountainmadness2020.bathroom.Checkpoint;
+
+import java.util.ArrayList;
+
+public class Stats {
+    private ArrayList<Checkpoint> checkpoints;
+
+    public Stats(ArrayList<Checkpoint> checkpoints) {
+        this.checkpoints = checkpoints;
+    }
+
+    public double currentScore() {
+        double returnScore = 0;
+        for (Checkpoint x : checkpoints) {
+            returnScore += x.score();
+        }
+        return returnScore;
+    }
+    public double totalTime() {
+        double returnValue = 0;
+        for (Checkpoint x : checkpoints) {
+            returnValue += x.totalTime();
+        }
+        return returnValue;
+    }
+    public double totalDistance() {
+        double returnValue = 0;
+        for (Checkpoint x : checkpoints) {
+            returnValue += x.totalDistance();
+        }
+        return returnValue;
+    }
+    public ArrayList<String> words() {
+        ArrayList<String> returnWords = new ArrayList<>();
+        for (Checkpoint x : checkpoints) {
+            returnWords.add(x.currentWord());
+        }
+        return returnWords;
+    }
+    public boolean search(Checkpoint checkpoint) {
+        return words().contains(checkpoint.currentWord());
+    }
+}
